64d772ea89a97630670f6f966d1181b75745e22a
Luis <luissolivaresp@gmail.com>
2022-10-18 14:09:12 -0500

    Creacion estructura del proyecto

---
+++ src/pages/Clients_products_page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Clients_products_page = () => {
+  return (
+    <div>
+        
+        Clients_products_page
+
+
+
+
+    </div>
+  )
+}
+
+export default Clients_products_page;
