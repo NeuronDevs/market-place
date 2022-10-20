6acbb39b012ff11e02389467332aefbe7789ffd0
Luis <luissolivaresp@gmail.com>
2022-10-20 14:17:03 -0500

    implementacion de react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Clients_products_page from './pages/Clients_products_page';
+import { NotFound } from './pages/NotFound';
 function App() {
   return (
-    <div className="">
+
+    <BrowserRouter>
       <Header></Header>
-      <Clients_products_page />
+      <Routes>
+        <Route path='/' element={<Clients_products_page />}></Route>
+        <Route path='/about' element={<div>about</div>}></Route>
+        <Route path='/productos' element={<Clients_products_page />}></Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
       <Footer></Footer>
-    </div>
+
+
+
+
+    </BrowserRouter>
+
   );
 }
 
