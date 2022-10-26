b7bb9e240874226b1390c39f678ce29634beef48
sergioqp82 <sergioalejandroquintero82@gmail.com>
2022-10-26 18:40:33 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import New_product_page from './pages/New_product_page';
 import Clients_products_page from './pages/Clients_products_page';
-import New_product_page from './pages/New_product_page';
-import { NotFound } from './pages/NotFound';
 import Sales_page from './pages/Sales_page';
+import { NotFound } from './pages/NotFound';
 function App() {
   return (
 
