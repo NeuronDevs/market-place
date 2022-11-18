22e0064da91a4b2d019093591d8c3196f7e86943
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2022-11-18 10:34:51 -0500

    Pagina principal de Admin

--- src/components/ProductAdmin.jsx
+++ src/components/ProductAdmin.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
+import { ConfirmModal } from './ConfirmModal'
 
-export const ProductAdmin = ({ id, name, price, img, lot }) => {
+export const ProductAdmin = (props) => {
 
 
   return (
     <div className="col">
       <div className="card m-2" >
         <div className='img-container'>
-          <img src={img} className="card-img-top card-img-style" alt="..." />
+          <img src={props.img} className="card-img-top card-img-style" alt="..." />
         </div>
         <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">{price}</p>
+          <h5 className="card-title">{props.name}</h5>
+          <p className="card-text">{props.price}</p>
           <button type="button" className="btn btn-primary">
             Modificar
           </button>
