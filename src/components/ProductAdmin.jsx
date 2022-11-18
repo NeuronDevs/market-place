3b954c5ba34c96fbe84afec87ac861c8048a810c
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2022-11-18 13:58:51 -0500

    Boton editar, eliminar, añadir Vista Admin

--- src/components/ProductAdmin.jsx
+++ src/components/ProductAdmin.jsx
@@ -1,23 +1,36 @@
 import React, { useState } from 'react'
-import { ConfirmModal } from './ConfirmModal'
+import { Button } from '@material-ui/core';
+import { Stack } from '@mui/system';
+import { Delete } from '@material-ui/icons';
+import { Edit } from '@material-ui/icons';
+
 
 export const ProductAdmin = (props) => {
 
 
   return (
-    <div className="col">
-      <div className="card m-2" >
-        <div className='img-container'>
-          <img src={props.img} className="card-img-top card-img-style" alt="..." />
-        </div>
-        <div className="card-body">
-          <h5 className="card-title">{props.name}</h5>
-          <p className="card-text">{props.price}</p>
-          <button type="button" className="btn btn-primary">
-            Modificar
-          </button>
+    
+      <div className="col">
+ 
+        <div className="card m-2" >
+          <div className='img-container'>
+            <img src={props.img} className="card-img-top card-img-style" alt="..." />
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{props.name}</h5>
+            <p className="card-text">{props.price}</p>
+            
+            <Stack direction="row" spacing={1} className='justify_text'>
+              <Button variant='contained' color="primary" startIcon={<Edit />}>
+              Editar
+              </Button>
+              <Button variant='contained' color="secondary" startIcon={<Delete />}>
+              Eliminar
+              </Button>
+            </Stack>
+          </div>
         </div>
       </div>
-    </div>
+
   )
 }
--- src/pages/Admin_view_products.jsx
+++ src/pages/Admin_view_products.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { AdminListProduct } from '../components/AdminListProduct';
+import { Button } from '@material-ui/core'
+import { AddCircle } from '@material-ui/icons';
+
 
 const Admin_view_products= () => {
   return (
    <div className='p-4 container'>
+    <div className="text_rigth">
+        <Button variant='contained' color="primary"  endIcon={<AddCircle />}>
+        Añadir
+        </Button>
+    </div>
     <h4>Administrador - Lista de Productos</h4>
     <AdminListProduct></AdminListProduct>    
    </div>
