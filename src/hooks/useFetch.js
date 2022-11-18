f883c45ccdb17e314d1b6739c5f27d931ccf1c4d
Luis <luissolivaresp@gmail.com>
2022-11-18 00:10:08 -0500

    merge updaates

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
-function useFetch(ruta,isAdmin=false) {
-    isAdmin=isAdmin?true:'same-origin';
+function useFetch(ruta, isAdmin = false) {
+    isAdmin = isAdmin ? true : 'same-origin';
     const [isLoading, setIsLoading] = useState(true);
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
         const loadData = async () => {
-            const result = await fetch(ruta,{credentials:true})
+            const result = await fetch(ruta, { credentials: true })
                 .then(response => response.json())
                 .then(datos => datos)
+            console.log(result);
             setProducts(result);
             setIsLoading(false);
         }
